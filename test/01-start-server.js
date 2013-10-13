fa87b3f113cfb71d0e28ab47f7bd9849ab5a1f51
Timothy Overly <timothy.overly@gmail.com>
2013-10-13 09:23:31 -0400

    Much better testing, learned how to set up that framework

--- test/01-start-server.js
+++ test/01-start-server.js
@@ -7,10 +7,12 @@ console.log("FYI: You need a mongodb server running for these tests to work.")
 before(function(done) {
     require('../lib/apply').Apply("test");
     var collections = ["jobs", "applications"]
-
     var db = mongojs.connect("test", collections);
-    
-    db.jobs.insert({position:"test position",description:"test description"}, function(err, job) {
+    db.jobs.runCommand('drop', function(err, res) {
+      // console.log(res);
+    });    
+    db.applications.runCommand('drop', function(err, res) {
+      // console.log(res);
     });
     done();
 });
--- test/application-test.js
+++ test/application-test.js
@@ -4,26 +4,65 @@ var client = restify.createJsonClient({
     url: 'http://0.0.0.0:8080'
 });
  
-describe('service: applications', function() {
-     describe('get response check', function() {
-        it('should not get a 200 response', function(done) {
-            client.get('/applications', function(err, req, res, obj) {
-                if (res.statusCode == 200) {
-                    throw new Error('invalid response from /applications');
-                }
-                done();
-            });
-        });
-    });
-});
+var jobId = "NEW"
 
-describe('post response check', function() {
-  it('should get a 400 response because missing description', function(done) {
-    client.post('/applications', { application: { invalid: 'object' } }, function(err, req, res, obj) {
-      if (res.statusCode != 400) {
-        throw new Error('invalid response from /applications');
-      }
-      done();
+describe('service: applications', function() {
+
+  beforeEach(function(done){
+    var collections = ["jobs", "applications"]
+    var db = mongojs.connect("test", collections);
+     db.jobs.insert({position:"test position",description:"test description"}, function(err, job) {
+        jobId = job[0]._id
+        done();
+     });
+  });
+
+  after(function(done){
+    done();
+  });
+
+// beforeEach(){
+//     db.jobs.insert({position:"test position",description:"test description"}, function(err, job) {
+//     });
+// }
+
+  describe('get response check', function() {
+    it('should not get a 200 response', function(done) {
+      client.get('/applications', function(err, req, res, obj) {
+          if (res.statusCode == 200) {
+              throw new Error('invalid response from /applications');
+          }
+          done();
+      });
+    });
+  });
+
+  describe('post response check', function() {
+    it('should get a 400 response because missing description', function(done) {
+      client.post('/applications', { application: { invalid: 'object' } }, function(err, req, res, obj) {
+        if (res.statusCode != 400) {
+          throw new Error('invalid response from /applications');
+        }
+        done();
+      });
+    });
+
+    it('should get a 400 response with missing jobId', function(done) {
+      client.post('/applications', { application: { name: 'my name', justification: "I am really nice.", code:"https://github.com/tgsoverly/apply"} }, function(err, req, res, obj) {
+        if (res.statusCode != 400) {
+          throw new Error('invalid response from /applications');
+        }
+        done();
+      });
+    });
+
+    it('should get a 200 response with valid application', function(done) {
+      client.post('/applications', { application: { name: 'my name', justification: "I am really nice.", code:"https://github.com/tgsoverly/apply", jobId:jobId } }, function(err, req, res, obj) {
+        if (res.statusCode != 200) {
+          throw new Error('invalid response from /applications');
+        }
+        done();
+      });
     });
   });
 });
